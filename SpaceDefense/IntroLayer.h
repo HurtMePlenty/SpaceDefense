f07cf2aee806a8139186bc27751a081770d7ab2f
Eliphas <eliphas@Mac-mini-Eliphas.local>
2013-10-11 18:02:58 +0400

    Initial Commit

---
+++ SpaceDefense/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  SpaceDefense
+//
+//  Created by Eliphas on 11.10.13.
+//  Copyright Eliphas 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ SpaceDefense/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SpaceDefense
+//
+//  Created by Eliphas on 11.10.13.
+//  Copyright Eliphas 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
